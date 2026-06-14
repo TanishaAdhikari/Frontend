c4029b885674b2d219d574a84096e4f81045e20c
_taneesha_21 <itschhaya19k@gmail.com>
2026-06-14 14:08:52 +0545

    Add files via upload

---
+++ Father.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Father = ({name,surname,age}) => {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Surname: {surname}</p>
+      <p>Age: {age}</p>
+    </div>
+  )
+}
+
+export default Father
